012317cc5b9f6de0bbedbef2d0494ffd84ac7ac2
Rosangela <96451367+Rommendes@users.noreply.github.com>
2023-07-13 11:55:48 -0300

    commits

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,8 +1,12 @@
 import './Colaborador.css'
 
-const Colaborador = ({nome, imagem, cargo, corDeFundo})=> {
+const Colaborador = ({nome, imagem, cargo, corDeFundo, aoDeletar})=> {
     return (
         <div  className='colaborador'>
+
+        <div className='deletar' onClick={aoDeletar}> Deletar 
+        </div>
+
             <div className='cabecalho' style={{background: corDeFundo}}>
                 <img src= {imagem} alt= {nome} />
             </div>
